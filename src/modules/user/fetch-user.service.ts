18d119cadc850c65126dfe52f2878c0b5a694933
Erick Malta <erickmalta100@gmail.com>
2021-09-18 21:57:57 -0300

    feat(user): map user-comments related routes

--- src/modules/user/fetch-user.service.ts
+++ src/modules/user/fetch-user.service.ts
@@ -3,6 +3,8 @@ import axios, { AxiosInstance } from 'axios';
 import { devguildUserServiceUrl } from 'src/config';
 import { Technology } from '../project/interfaces/technology.interface';
 import { ProjectMembers } from './interfaces/project-members';
+import { UserComments } from './interfaces/user-comments';
+import { UserCommentsWithUser } from './interfaces/user-comments-with-user';
 import { UserWithTechnology } from './interfaces/user-with-technology';
 import { User } from './interfaces/user.interface';
 
@@ -17,13 +19,13 @@ export class FetchUsersService {
   }
 
   async getUserById(id: string): Promise<User> {
-    const { data } = await this.httpService.get(`/users/${id}`);
+    const { data } = await this.httpService.get<User>(`/users/${id}`);
 
     return data;
   }
 
   async getUserByIds(ids: string): Promise<User[]> {
-    const { data } = await this.httpService.get('/users', {
+    const { data } = await this.httpService.get<User[]>('/users', {
       params: ids,
     });
 
@@ -31,31 +33,51 @@ export class FetchUsersService {
   }
 
   async getUserByGithubId(id: number): Promise<User> {
-    const { data } = await this.httpService.get(`/users/${id}/github`);
+    const { data } = await this.httpService.get<User>(`/users/${id}/github`);
 
     return data;
   }
 
   async getUsersByTechnologyId(id: string): Promise<UserWithTechnology[]> {
-    const { data } = await this.httpService.get(`/users/technologies/${id}`);
+    const { data } = await this.httpService.get<UserWithTechnology[]>(
+      `/users/technologies/${id}`,
+    );
+
+    return data;
+  }
+
+  async getUserCommentsByProjectId(
+    id: string,
+  ): Promise<UserCommentsWithUser[]> {
+    const { data } = await this.httpService.get<UserCommentsWithUser[]>(
+      `/projects/${id}/comments`,
+    );
 
     return data;
   }
 
   async getProjectMembers(id: string): Promise<ProjectMembers[]> {
-    const { data } = await this.httpService.get(`/projects/${id}/members`);
+    const { data } = await this.httpService.get<ProjectMembers[]>(
+      `/projects/${id}/members`,
+    );
 
     return data;
   }
 
   async createNewUser(body: any): Promise<User> {
-    const { data } = await this.httpService.post('/users', body);
+    const { data } = await this.httpService.post<User>('/users', body);
 
     return data;
   }
 
-  async insertProjectMember(id: string, body: any): Promise<void> {
-    const { data } = await this.httpService.post(
+  async updateUser(id: string, body: any): Promise<User> {
+    const { data } = await this.httpService.put<User>(`/users/${id}`, body);
+
+    return data;
+  }
+
+  async insertProjectMember(id: string, body: any): Promise<null> {
+    const { data } = await this.httpService.post<null>(
       `/projects/${id}/members`,
       body,
     );
@@ -63,8 +85,27 @@ export class FetchUsersService {
     return data;
   }
 
+  async insertComment(
+    id: string,
+    body: any,
+    user_id: string,
+  ): Promise<UserComments> {
+    const { data } = await this.httpService.post<UserComments>(
+      `projects/${id}/comments`,
+      {
+        ...body,
+        user_id,
+      },
+    );
+
+    return data;
+  }
+
   async insertTechnologiesInUser(id: string, body: any): Promise<Technology[]> {
-    const { data } = await this.httpService.post(`/users/${id}/techs`, body);
+    const { data } = await this.httpService.post<Technology[]>(
+      `/users/${id}/techs`,
+      body,
+    );
 
     return data;
   }
